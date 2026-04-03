5d01d763afed1b21fea96cf422e99ef192a1c4be
Eddy Yail De Oleo Arias <20242152@itla.edu.do>
2026-04-03 17:32:02 -0400

    automatización HU-02 con Selenium, reportes y capturas para registro de productos

--- EmpanadaInventory.Tests/Tests/BaseTest.cs
+++ EmpanadaInventory.Tests/Tests/BaseTest.cs
@@ -1,5 +1,4 @@
 using AventStack.ExtentReports;
-using AventStack.ExtentReports.Reporter;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
@@ -11,22 +10,11 @@ namespace EmpanadaInventory.Tests
     public class BaseTest
     {
         protected IWebDriver Driver = null!;
-        protected ExtentReports Extent = null!;
         protected ExtentTest Test = null!;
         protected string BaseUrl = "http://localhost:5000";
-        private static string ReportPath = Path.Combine(
-            AppDomain.CurrentDomain.BaseDirectory, "Reportes", "Reporte.html");
 
-        [OneTimeSetUp]
-        public void IniciarReporte()
-        {
-            Directory.CreateDirectory(Path.GetDirectoryName(ReportPath)!);
-            var reporter = new ExtentSparkReporter(ReportPath);
-            reporter.Config.DocumentTitle = "Reporte QA - Empanada Inventory";
-            reporter.Config.ReportName = "Pruebas Automatizadas";
-            Extent = new ExtentReports();
-            Extent.AttachReporter(reporter);
-        }
+        // Usa el reporte global compartido
+        protected ExtentReports Extent => ReporteManager.Extent;
 
         [SetUp]
         public void IniciarDriver()
@@ -44,28 +32,18 @@ namespace EmpanadaInventory.Tests
             Driver?.Dispose();
         }
 
-        [OneTimeTearDown]
-        public void CerrarReporte()
-        {
-            Extent.Flush();
-            Console.WriteLine($"Reporte generado en: {ReportPath}");
-        }
-
-        // Espera explícita: espera hasta que un elemento sea visible
         protected IWebElement Esperar(By locator, int segundos = 10)
         {
             var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(segundos));
             return wait.Until(ExpectedConditions.ElementIsVisible(locator));
         }
 
-        // Espera explícita: espera hasta que un elemento sea clickeable
         protected IWebElement EsperarClickeable(By locator, int segundos = 10)
         {
             var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(segundos));
             return wait.Until(ExpectedConditions.ElementToBeClickable(locator));
         }
 
-        // Espera explícita: espera hasta que la URL contenga un texto
         protected void EsperarUrl(string fragmento, int segundos = 10)
         {
             var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(segundos));
